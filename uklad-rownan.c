f6298cea55af40c5b2fd37f111966b2d25952ce8
Radek <radek.nimal@gmail.com>
2017-11-23 08:55:55 +0100

    dodano ignore

--- uklad-rownan.c
+++ uklad-rownan.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+int main ()
+{
+float a1,b1,c1,a2,b2,c2,W,Wx,Wy;
+printf("Witaj!\nTen program liczy uklad rownan metoda wyznacznikow.\n a1x + b1y = c1 \n a2x + b2y = c2 \n");
+int W(int a1, int b1, int c1)
+{
 
-int
+}
+}
