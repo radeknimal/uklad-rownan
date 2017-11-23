4c6d0f093cc0dfe086ec0843cb213ff844dfb91e
Radek <radek.nimal@gmail.com>
2017-11-23 10:42:39 +0100

    dzialajacy program

--- uklad-rownan.c
+++ uklad-rownan.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-int main ()
-{
-float a1,b1,c1,a2,b2,c2,W,Wx,Wy;
-printf("Witaj!\nTen program liczy uklad rownan metoda wyznacznikow.\n a1x + b1y = c1 \n a2x + b2y = c2 \n");
-int W(int a1, int b1, int c1)
-{
-
+void wczytaj(double *a1, double *a2, double *b1,
+	 double *b2, double *c1, double *c2){
+	puts("Podaj wspolczynnik a1"); scanf("%lf",a1);
+	puts("Podaj wspolczynnik b1"); scanf("%lf",b1);
+	puts("Podaj wspolczynnik c1"); scanf("%lf",c1);
+	puts("Podaj wspolczynnik a2"); scanf("%lf",a2);
+	puts("Podaj wspolczynnik b2"); scanf("%lf",b2);
+	puts("Podaj wspolczynnik c2"); scanf("%lf",c2);
 }
+double wyznacznik_glowny(double a1, double a2, double b1, double b2)
+	{return a1*b2-b1*a2;}
+double wyznacznik_x(double c1, double c2, double b1, double b2)
+        {return c1*b2-b1*c2;}
+double wyznacznik_y(double a1, double a2, double c1, double c2)
+        {return a1*c2-c1*a2;}
+int main (){
+puts("Witaj!\nTen program liczy uklad rownan metoda wyznacznikow.\n / a1x + b1y = c1 \n \\ a2x + b2y = c2 \n");
+double a1,a2,b1,b2,c1,c2;
+double W,Wx,Wy;
+double x,y;
+wczytaj (&a1,&a2,&b1,&b2,&c1,&c2);
+W = wyznacznik_glowny(a1,b1,a2,b2);
+Wx = wyznacznik_x(b1,b2,c1,c2);
+Wy = wyznacznik_y(a1,c1,a2,c2);
+x=Wx/W;
+y=Wy/W;
+
+printf("x = %lf\ny = %lf\n", x, y);
+return 0;
 }
